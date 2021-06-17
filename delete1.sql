037c6c6fecfb25273d0af4b4acc8dbd7a278180e
Grandhi Charith <srisaicharith_b180987me@nitc.ac.in>
2021-06-17 15:14:38 +0530

    addds subquery

--- delete1.sql
+++ delete1.sql
@@ -1,3 +1,3 @@
-DELETE FROM books_subjects WHERE subject=subjects.id and subject.name='History';
+DELETE FROM books_subjects WHERE subject in (SELECT bs.subject FROM books_subject bs,subjects s WHERE bs.subject=s.id AND s.name='History');
 
 DELETE FROM subjects where name='History';
